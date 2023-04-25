0250e12ad1cb80b560577861d4d2af49b19abdd4
mxy <1311757233@qq.com>
2023-04-25 20:38:01 +0800

    每日健康申报完成

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import json
 import os
 from urllib.parse import urljoin
-
+from apscheduler.schedulers.background import BackgroundScheduler
+import datetime
 import pandas as pd
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS, cross_origin
@@ -14,6 +15,17 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 UPLOAD_FOLDER = './attachment'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def job():
+    sql = "update studnet set tag='false'"
+    con = UseSQLServer()
+    con.update_mssql_data(sql)
+
+
+scheduler = BackgroundScheduler(daemon=True)
+scheduler.add_job(job, 'cron', hour=0, minute=0)
+scheduler.start()
 
 
 @app.route('/login', methods=['get'])
@@ -55,7 +67,6 @@ def change_passwd():
 @app.route('/get/data', methods=['GET'])
 def get_data():
     sql = request.args.get('sql')
-    print(sql)
     con = UseSQLServer()
     df = con.get_mssql_data(sql)
     return jsonify(code=200, data=df.fillna('').to_dict('records'), msg="success")
@@ -85,13 +96,15 @@ def delete():
     else:
         return jsonify(code=404, msg="操作失败")
 
+
 @app.route('/see', methods=['get'])
 def see():
     id = request.args.get('id')
     con = UseSQLServer()
-    sql=f"select * from process_item where process_id={id}"
-    df=con.get_mssql_data(sql)
+    sql = f"select * from process_item where process_id={id}"
+    df = con.get_mssql_data(sql)
     return jsonify(code=200, data=df.fillna('').to_dict('records'), msg="success")
+
 
 @app.route('/upload/data', methods=['POST'])
 def upload_data():
@@ -125,48 +138,54 @@ def student_health_record_get():
     else:
         return jsonify(code=404, msg='未找到资源'), 404
 
+
 @app.route('/ask_for_leave/preview', methods=['get'])
 def ask_for_leave_preview():
-    con=UseSQLServer()
-    id=request.args.get('id')
-    sql=f"select a.start_time,a.end_time,a.reason from ask_for_leave a where a.id={id}"
-    df=con.get_mssql_data(sql)
-    return jsonify(code=200, msg="success",data=df.fillna('').to_dict('records'))
+    con = UseSQLServer()
+    id = request.args.get('id')
+    sql = f"select a.start_time,a.end_time,a.reason from ask_for_leave a where a.id={id}"
+    df = con.get_mssql_data(sql)
+    return jsonify(code=200, msg="success", data=df.fillna('').to_dict('records'))
+
 
 @app.route('/ask_for_leave/add', methods=['POST'])
 def ask_for_leave_add():
-    con=UseSQLServer()
+    con = UseSQLServer()
     data = request.form
     files = request.files.getlist('file')
-    username=data['username']
-    name=data['name']
-    reason=data['reason']
-    start_time=data['start_time']
-    end_time =data['end_time']
-    df1=pd.DataFrame()
-    df1=df1.append({"username":username,"name":name,'reason':reason,'start_time':start_time,'end_time':end_time}, ignore_index=True)
+    username = data['username']
+    name = data['name']
+    reason = data['reason']
+    start_time = data['start_time']
+    end_time = data['end_time']
+    df1 = pd.DataFrame()
+    df1 = df1.append(
+        {"username": username, "name": name, 'reason': reason, 'start_time': start_time, 'end_time': end_time},
+        ignore_index=True)
     con.write_table('ask_for_leave', df1)
-    sql='SELECT MAX(id) as id FROM ask_for_leave'
-    df=con.get_mssql_data(sql)
+    sql = 'SELECT MAX(id) as id FROM ask_for_leave'
+    df = con.get_mssql_data(sql)
     id = df.iloc[0]['id']
-    return upload(files,id,'请假',con,app.root_path)
+    return upload(files, id, '请假', con, app.root_path)
+
 
 @app.route('/ask_for_leave/delete', methods=['POST'])
 def ask_for_leave_delete():
     val = json.loads(request.get_data())
-    con=UseSQLServer()
-    id=val['id']
-    sql=f"select file_url from file_table where original_id='{id}' and type=N'请假'"
-    df=con.get_mssql_data(sql)
+    con = UseSQLServer()
+    id = val['id']
+    sql = f"select file_url from file_table where original_id='{id}' and type=N'请假'"
+    df = con.get_mssql_data(sql)
     files = df['file_url'].to_numpy()
     sql = f"DELETE FROM ask_for_leave WHERE id={id}"
-    con=UseSQLServer()
-    del_file(files,id,'请假',con,app.root_path)
-    df=con.update_mssql_data(sql)
-    if df == 'success' :
+    con = UseSQLServer()
+    del_file(files, id, '请假', con, app.root_path)
+    df = con.update_mssql_data(sql)
+    if df == 'success':
         return jsonify(code=200, msg=df)
     else:
         return jsonify(code=404, msg="删除失败")
+
 
 @app.route('/student/health_record/update', methods=['POST'])
 def student_health_record_update():
@@ -181,71 +200,99 @@ def student_health_record_update():
     else:
         return jsonify(code=404, msg="更新失败")
 
+
 @app.route('/attachment/<path:filename>')
 def get_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-@app.route('/old_file/get',methods=['get'])
+
+@app.route('/old_file/get', methods=['get'])
 def old_file_get():
-    original_id=request.args.get('original_id')
-    type=request.args.get('type')
-    sql=f"select file_name,file_url from file_table where type=N'{type}' and original_id='{original_id}'"
+    original_id = request.args.get('original_id')
+    type = request.args.get('type')
+    sql = f"select file_name,file_url from file_table where type=N'{type}' and original_id='{original_id}'"
     print(sql)
-    con=UseSQLServer()
-    df=con.get_mssql_data(sql)
+    con = UseSQLServer()
+    df = con.get_mssql_data(sql)
     return jsonify(code=200, msg="success", data=df.fillna('').to_dict('records'))
+
 
 @app.route('/ask_for_leave/update', methods=['POST'])
 def ask_for_leave_update():
-    con=UseSQLServer()
+    con = UseSQLServer()
     data = request.form
-    id=data['id']
-    reason=data['reason']
-    start_time=data['start_time']
-    end_time =data['end_time']
-    old_file=eval(data['old_file'])
-    old_file_string="','".join(old_file)
-    new_file=request.files.getlist('file')
+    id = data['id']
+    reason = data['reason']
+    start_time = data['start_time']
+    end_time = data['end_time']
+    old_file = eval(data['old_file'])
+    old_file_string = "','".join(old_file)
+    new_file = request.files.getlist('file')
     sql = f"UPDATE ask_for_leave SET reason = '{reason}', start_time = '{start_time}', end_time = '{end_time}' " \
           f"WHERE id = {id}"
     con.update_mssql_data(sql)
-    return upload_update(id,'请假',old_file_string,new_file,con,app.root_path)
+    return upload_update(id, '请假', old_file_string, new_file, con, app.root_path)
 
-@app.route('/student/info/get',methods=['get'])
+
+@app.route('/student/info/get', methods=['get'])
 def student_info_get():
-    username=request.args.get('username')
-    sql=f"select * from student where name=N'{username}'"
-    con=UseSQLServer()
-    df=con.get_mssql_data(sql)
+    username = request.args.get('username')
+    sql = f"select * from student where name=N'{username}'"
+    con = UseSQLServer()
+    df = con.get_mssql_data(sql)
     return jsonify(code=200, msg="success", data=df.fillna('').to_dict('records'))
 
-@app.route('/process/submit',methods=['POST'])
+
+@app.route('/process/submit', methods=['POST'])
 def process_submit():
     con = UseSQLServer()
     val = json.loads(request.get_data())
-    type=val['type']
-    username=val['username']
-    sql=f"insert process(type,username) values(N'{type}','{username}')"
+    type = val['type']
+    username = val['username']
+    sql = f"insert process(type,username) values(N'{type}','{username}')"
     con.update_mssql_data(sql)
-    sql=f"select max(id) process_id from process "
-    df1=con.get_mssql_data(sql)
-    df=pd.DataFrame(val['table'])
-    df['process_id']=df1.iloc[0]['process_id']
+    sql = f"select max(id) process_id from process "
+    df1 = con.get_mssql_data(sql)
+    df = pd.DataFrame(val['table'])
+    df['process_id'] = df1.iloc[0]['process_id']
     con.write_table('process_item', df.fillna(''))
     return jsonify(code=200, msg="success")
 
-@app.route('/process/update',methods=['post'])
+
+@app.route('/process/update', methods=['post'])
 def update():
     val = json.loads(request.get_data())
-    id=val['id']
+    id = val['id']
     df = pd.DataFrame(val['table'])
-    sql=f"delete from process_item where process_id={id}"
+    sql = f"delete from process_item where process_id={id}"
     con = UseSQLServer()
     con.update_mssql_data(sql)
-    df = df.loc[:, ['item1', 'item2','item3','item4','item5','item6']]
+    df = df.loc[:, ['item1', 'item2', 'item3', 'item4', 'item5', 'item6']]
     df['process_id'] = id
     con.write_table('process_item', df.fillna(''))
     return jsonify(code=200, msg="success")
+
+@app.route('/stu/submit', methods=['GET'])
+def submit():
+    mysql_connect = UseSQLServer()
+    username = request.args.get('username')
+    body_condition = request.args.get('body_condition')
+    is_infection = request.args.get('is_infection')
+    temperature = request.args.get('temperature')
+    location = request.args.get('location')
+    infection_count = request.args.get('infection_count')
+    other_condition = request.args.get('other_condition')
+    sql = "INSERT INTO health_declartion (username, is_infection, temperature, location, body_condition,infection_count," \
+          f"other_condition) VALUES ('{username}', '{is_infection}', '{temperature}', {location}, '{body_condition}'," \
+          f" '{infection_count}','{other_condition}');"
+    df = mysql_connect.update_mssql_data(sql)
+    sql=f"update student set tag='true' where username='{username}'"
+    df = mysql_connect.update_mssql_data(sql)
+    if df == 'success':
+        return jsonify(code=200, msg=df)
+    else:
+        return jsonify(code=404, msg="insert failed")
+
 
 
 if __name__ == '__main__':
